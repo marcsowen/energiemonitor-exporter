2d53091841bade3f5225b1d1ecc0f6f847ddee14
Marc Sowen <marc@sowen.net>
2024-02-21 10:25:49 +0100

    Added ability to add more than one region code

--- energiemonitor-exporter.py
+++ energiemonitor-exporter.py
@@ -6,37 +6,41 @@ import time
 import requests
 from prometheus_client import start_http_server, Gauge
 
-autarky = Gauge('autarky', "Autoarky of region")
-energy_mix = Gauge('energy_mix', 'Energy mix of region')
-num_installations = Gauge('num_installations', 'Number of installations', ['type', 'name'])
-usage = Gauge('usage', 'Current amount per time period in kWh', ['type', 'name'])
-installed_capacity = Gauge('installed_capacity', 'Total installed capacity', ['type', 'name'])
+autarky = Gauge('autarky', "Autoarky of region", ['region_code'])
+energy_mix = Gauge('energy_mix', 'Energy mix of region', ['region_code'])
+num_installations = Gauge('num_installations', 'Number of installations', ['region_code', 'type', 'name'])
+usage = Gauge('usage', 'Current amount per time period in kWh', ['region_code', 'type', 'name'])
+installed_capacity = Gauge('installed_capacity', 'Total installed capacity', ['region_code', 'type', 'name'])
 
 if __name__ == '__main__':
-    print("EnergieMonitor exporter v0.1\n")
-    region_code = '03355'
+    print("EnergieMonitor exporter v0.2\n")
+    region_codes = ['03355', '03355022']
     server_port = 3325
 
-    print("Region code: " + str(region_code) + "\n")
-    print("Port       : " + str(server_port) + "\n")
+    print("Region codes: " + str(region_codes) + "\n")
+    print("Port        : " + str(server_port) + "\n")
 
     start_http_server(server_port)
     while True:
-        response = json.loads(requests.get('https://api-energiemonitor.eon.com/meter-data?regionCode=' + region_code)
-                              .content.decode('UTF-8'))
+        for rg_id in region_codes:
+            response = json.loads(requests.get('https://api-energiemonitor.eon.com/meter-data?regionCode=' + rg_id)
+                                  .content.decode('UTF-8'))
 
-        autarky.set(response['autarky'])
-        energy_mix.set(response['energyMix'])
+            autarky.labels(region_code=rg_id).set(response['autarky'])
+            energy_mix.labels(region_code=rg_id).set(response['energyMix'])
 
-        for consumption in response['consumptions']['list']:
-            name = consumption['name']
-            num_installations.labels(type='consumption', name=name).set(consumption['numberOfInstallations'])
-            usage.labels(type='consumption', name=name).set(consumption['usage'])
+            for consumption in response['consumptions']['list']:
+                name = consumption['name']
+                (num_installations.labels(region_code=rg_id, type='consumption', name=name).set(
+                    consumption['numberOfInstallations']))
+                usage.labels(region_code=rg_id, type='consumption', name=name).set(consumption['usage'])
 
-        for feed_in in response['feedIn']['list']:
-            name = feed_in['name']
-            num_installations.labels(type='feed_in', name=name).set(feed_in['numberOfInstallations'])
-            usage.labels(type='feed_in', name=name).set(feed_in['usage'])
-            installed_capacity.labels(type='feed_in', name=name).set(feed_in['installedCapacity'])
+            for feed_in in response['feedIn']['list']:
+                name = feed_in['name']
+                num_installations.labels(region_code=rg_id, type='feed_in', name=name).set(
+                    feed_in['numberOfInstallations'])
+                usage.labels(region_code=rg_id, type='feed_in', name=name).set(feed_in['usage'])
+                installed_capacity.labels(region_code=rg_id, type='feed_in', name=name).set(
+                    feed_in['installedCapacity'])
 
         time.sleep(300)
